d97249016e0b2ccb50ab48da021c4816aca4c31b
Andrey Gubarev <andrey@andreygubarev.com>
2023-06-06 20:55:12 +0400

    add test

--- roles/tailscale/molecule/default/tests/test_default.py
+++ roles/tailscale/molecule/default/tests/test_default.py
@@ -1,10 +1,13 @@
 """Role testing files using testinfra."""
 
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
+def test_tailscale_is_installed(host):
+    """Check if tailscale is installed."""
+    assert host.package("tailscale").is_installed
 
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
+
+def test_tailscale_running_and_enabled(host):
+    """Check if tailscale is running and enabled."""
+    tailscale = host.service("tailscaled")
+    assert tailscale.is_running
+    assert tailscale.is_enabled
