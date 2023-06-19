835e964c36d2eeee86db55d770e673637f9b51a5
Andrey Gubarev <andrey@andreygubarev.com>
2023-06-19 18:56:45 +0400

    add tests

--- roles/headscale/molecule/default/tests/test_default.py
+++ roles/headscale/molecule/default/tests/test_default.py
@@ -1,10 +1,18 @@
 """Role testing files using testinfra."""
 
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
+def test_headscale_binary(host):
+    """Validate headscale installation."""
+    f = host.file("/usr/bin/headscale")
 
     assert f.exists
     assert f.user == "root"
     assert f.group == "root"
+    assert f.mode == 0o755
+
+
+def test_headscale_port(host):
+    """Validate headscale port."""
+    s = host.socket("tcp://127.0.0.1:8514")
+
+    assert s.is_listening
